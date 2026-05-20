23ed5364a11e41bbc00e1f7700a8462ec5101a72
Sri Valli Deepthi Reddy <24B11EC163@adityauniversity.in>
2026-05-20 23:03:09 +0530

    Create 35_SearchInsertPosition.py

---
+++ 35_SearchInsertPosition.py
@@ -0,0 +1,22 @@
+# Binary search
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if target == nums[mid]:
+                return mid
+            elif (target < nums[mid] and
+                (mid == 0 or target > nums[mid - 1])):
+                return mid
+            elif target > nums[mid] and mid == len(nums) - 1:
+                return mid + 1
+            elif target > nums[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
